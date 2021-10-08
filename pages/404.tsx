e46b5c667214e97f3b6e7760b61a913fa1148a59
Awais Iqbal <iamawaissiqball@gmail.com>
2021-10-09 01:47:06 +0500

    Fix: default export in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
-export const PageNotFound = () => {
+const PageNotFound = () => {
   return (
     <div>
       <h1>Idiot! Princes is not in this castle</h1>
     </div>
   );
 };
+
+export default PageNotFound;
