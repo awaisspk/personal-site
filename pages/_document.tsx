5966c86547080d881f029b7f8dda00d4c571db16
Awais Iqbal <iamawaissiqball@gmail.com>
2021-10-09 01:42:50 +0500

    remove next/script from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { getCssText } from "@stitchesConfig";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
